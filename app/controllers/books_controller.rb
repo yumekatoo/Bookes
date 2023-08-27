09432d105a79390b7d02d2bfe84dcd441b9f4211
EC2 Default User <ec2-user@ip-172-31-27-0.ap-southeast-2.compute.internal>
2023-08-27 04:20:25 +0000

    　課題提出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,22 @@
 class BooksController < ApplicationController
   def new
-    @books = Book.new
+    @book = Book.new
   end
   
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    redirect_to books_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
-    @books = Book.all 
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -24,9 +28,19 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  get 'books/:id' => 'books#show', as: 'book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get '/top' => 'homes#top'
+ resources :books
+root :to => 'homes#top'
 end
